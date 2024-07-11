b2c1cf84a04be3d0768bcce134c84e2a60d3dbe1
kelpsyberry <138107494+kelpsyberry@users.noreply.github.com>
2024-07-11 19:41:35 +0200

    Update to winit 0.30

--- imgui-winit-support/src/lib.rs
+++ imgui-winit-support/src/lib.rs
@@ -122,7 +122,7 @@ impl CursorSettings {
         match self.cursor {
             Some(mouse_cursor) if !self.draw_cursor => {
                 window.set_cursor_visible(true);
-                window.set_cursor_icon(to_winit_cursor(mouse_cursor));
+                window.set_cursor(to_winit_cursor(mouse_cursor));
             }
             _ => window.set_cursor_visible(false),
         }
